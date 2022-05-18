dbb3cb4775615179b41a540c3d83b86b50926b1d
DthRee523 <l928787694@gmail.com>
2022-05-19 00:42:58 +0800

    add message

--- core/nbbubblemessage.cpp
+++ core/nbbubblemessage.cpp
@@ -1,12 +1,40 @@
 #include "nbbubblemessage.h"
 
-NBBubbleMessage::NBBubbleMessage(QWidget *parent) : QWidget(parent){ messageInit(); }
+NBBubbleMessage::NBBubbleMessage(QListWidget *parent) : QListWidget(parent){ messageInit(NoSet); }
 
 /**
  * 初始化消息函数
  * @brief NBBubbleMessage::messageInit
  */
-void NBBubbleMessage::messageInit()
+void NBBubbleMessage::messageInit(NB nbItem)
+{
+    switch (nbItem)
+    {
+    case SetImage://Background Image 图片背景
+        break;
+    case SetColor://Background Color 纯色背景
+        break;
+    case NoSet:return;
+    default:return;
+    }
+}
+
+/**
+ * 重置窗口大小
+ * @brief NBBubbleMessage::paintEvent
+ * @param ev
+ */
+void NBBubbleMessage::paintEvent(QPaintEvent *ev)
 {
 
+}
+
+/**
+ * 放置背景图片
+ * @brief NBBubbleMessage::setBackgroundImage
+ * @param imagePath
+ */
+void NBBubbleMessage::setBackgroundImage(char *imagePath)
+{
+    emit imagePathSignal(imagePath);
 }
--- core/nbbubblemessage.h
+++ core/nbbubblemessage.h
@@ -7,17 +7,34 @@
 #define NBBUBBLEMESSAGE_H
 
 #include <QWidget>
+#include <QListWidget>
+#include <QDebug>
+#include <QPainter>
+#include <QPaintEvent>
 
-class NBBubbleMessage : public QWidget
+enum NB
+{
+    SetImage,       //设置背景图片
+    SetColor,       //设置纯色背景
+    NoSet,          //空
+    RomoteSenter,   //远程发送者
+    LocalSenter     //本地发送者
+};
+
+
+class NBBubbleMessage : public QListWidget
 {
     Q_OBJECT
 public:
-    explicit NBBubbleMessage(QWidget *parent = nullptr);
-
-    void messageInit();//初始化界面
+    explicit NBBubbleMessage(QListWidget *parent = nullptr);
+    void messageInit(NB nbItem);//初始化界面
+    void paintEvent(QPaintEvent *ev);//绘图重置事件
+    void setBackgroundImage(char* imagePath);//放置背景图片
+    void sendMessage(NB sender, char* text);//发送消息
+    void sendImage(NB sender, char* imageData);//发送图片
 
 signals:
-
+    void imagePathSignal(char* path);//放置图片信号
 };
 
 #endif // NBBUBBLEMESSAGE_H
